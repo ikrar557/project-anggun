346b8f75ec7e7990eb552e626666d0448a3e8783
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-11-01 23:48:56 +0700

    app engine setup

--- config/database.php
+++ config/database.php
@@ -1,6 +1,15 @@
 <?php
 
 use Illuminate\Support\Str;
+use Google\Cloud\SecretManager\V1\SecretManagerServiceClient;
+
+function getSecret($secretId) {
+    $client = new SecretManagerServiceClient();
+    $name = $client->secretVersionName('dicoding-backend-expert-439206', $secretId, 'latest');
+    $response = $client->accessSecretVersion($name);
+    return $response->getPayload()->getData();
+}
+
 
 return [
 
@@ -45,11 +54,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => getSecret('DB_HOST'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => getSecret('DB_DATABASE'),
+            'username' => getSecret('DB_USERNAME'),
+            'password' => getSecret('DB_PASSWORD'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
