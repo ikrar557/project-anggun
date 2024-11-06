657b32f81793dfe1ecabcc721f866bb9298f8672
Ikrar Bagaskara <ikrarb95@gmail.com>
2024-11-06 22:34:32 +0700

    test 3rd cd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,26 @@
 <?php
 
-namespace App\Providers;
-
-use Illuminate\Support\ServiceProvider;
+use Google\Cloud\SecretManager\V1\SecretManagerServiceClient;
+use Dotenv\Dotenv;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function register()
     {
-        //
+        if (env('GAE_APPLICATION_ENV')) {
+            $secretName = env('GOOGLE_ENV_SECRET_NAME');
+            $dotenv = $this->loadEnvFromSecret($secretName);
+            Dotenv::createImmutable(base_path(), '.env')->load();
+        }
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
+    private function loadEnvFromSecret($secretName)
     {
-        //
+        $client = new SecretManagerServiceClient();
+        $name = $client->secretVersionName('your-project-id', $secretName, 'latest');
+        $response = $client->accessSecretVersion($name);
+        $dotenvData = $response->getPayload()->getData();
+        file_put_contents(base_path('.env'), $dotenvData);
     }
 }
+
